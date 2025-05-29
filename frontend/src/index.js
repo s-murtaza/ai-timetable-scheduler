fb02a7660595b604b3d158c75914e9fc985809ab
s-murtaza <murtaza.naqvi.2003@gmail.com>
2025-05-29 14:00:38 +0500

    readying frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
+  </React.StrictMode>
+);
